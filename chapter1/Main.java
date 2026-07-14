1ed7cdc6653d248cbe473d0e2bad395a9d0718dc
marcoyimyaem <marcoyimyaem@gmail.com>
2026-07-14 04:01:21 +0000

    Day 13 - classes, properties, compiling, running, comments and javadoc

---
+++ chapter1/Main.java
@@ -0,0 +1,20 @@
+
+
+package chapter1;
+public class Main {
+    public static void main(String[] args) {
+        //create and instance of a Toy class = Toy object
+
+        /*   this
+        *    is a
+        * multiline 
+        *comment
+*/
+        Toy toy1 = new Toy();
+        toy1.name ="Rage pink";
+        toy1.brand = "Lab Vuvu";
+        toy1.price = 4500;
+        toy1.quantity = 12;
+        System.out.println();
+    }
+}
