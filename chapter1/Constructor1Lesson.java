4abdb92e9a8ad4a8c4abd42ff8c649389fa6f34b
marcoyimyaem <marcoyimyaem@gmail.com>
2026-07-16 03:59:14 +0000

    Day 15 - reading and writting objects, instance initializer blocks, and primitives

--- chapter1/Constructor1Lesson.java
+++ chapter1/Constructor1Lesson.java
@@ -16,3 +16,35 @@ class Chicken{
         name = "Duke";
     }
 }
+
+class Chick{ //🥚
+    public Chick(){
+        System.out.println("🐣 in constructor");
+    }
+    public void Chick(){
+        System.out.println("🐥💬 I'm a method");
+    }
+}
+
+class Swan{
+    int numEggs;
+    public static void main(String[] args) {
+        Swan mother = new Swan();
+        mother.numEggs = 4;//writting object fields
+        System.out.println(mother.numEggs); //reading object fields
+    }
+
+}
+class Name{
+    String first = "Theodore";
+    String last = "Moose";
+    String full = first+last;
+    public static void main(String[] args) {
+        Name name1 = new Name();
+        System.out.println(name1.full);
+        name1.first = "Marc";
+        name1.last = "Yim";
+        // name1.full = name1.first+name1.last;
+        System.out.println(name1.full);
+    }
+}
