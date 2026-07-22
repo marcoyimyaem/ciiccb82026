2ef92f917ed350e68c985540a791a8dc5d38df2d
marcoyimyaem <marcoyimyaem@gmail.com>
2026-07-22 03:59:47 +0000

    Day 20 - for loop structure, creating infinite loop, nested loops, label, break and continue statement.

--- chapter2/LoopsLesson.java
+++ chapter2/LoopsLesson.java
@@ -1,5 +1,7 @@
 package chapter2;
 
+import java.util.Scanner;
+
 public class LoopsLesson {
     
     public static void main(String[] args) {
@@ -11,5 +13,32 @@ public class LoopsLesson {
             System.out.println("do while : a = "+a--);
         while (a<10);
         System.out.println("end of program");
+        int b = 0;
+        boolean bol = true;
+        for( ;b<10;b++)
+            System.out.println("b = "+b++);
+
+        System.out.println("enter a valid number");
+        Scanner q = new Scanner(System.in);
+        int limit = q.nextInt();
+        for(int x=1;x<limit;x++){
+            for(int y=1;y<=x;y++){
+                System.out.print(x);
+            }
+            System.out.println();
+        }
+        String[] colors ={"black","red","pink","yellow","white","blue"}; 
+        for(String color: colors)
+            System.out.println("Color: "+color);
+        ROW_TABLE: for(int p = 1;p<=10;p++){
+        COL_TABLE:      for(int l = 1;l<=10;l++){
+                            if(l==5)
+                                // break ROW_TABLE;
+                                continue ROW_TABLE;
+                            System.out.print(l*p+"\t");
+                            }
+                        System.out.println();     
+
+        }
     }
 }
